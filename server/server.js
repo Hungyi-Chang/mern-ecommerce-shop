20ff03c782bc9060857bd41019b90ff164c240e9
Mark(Hung-Yi) Chang <mark.chang.private@gmail.com>
2021-08-13 22:46:13 +0800

    add herokuAwake to the server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
+import herokuAwake from 'heroku-awake';
 import path from 'path';
 import morgan from 'morgan';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
@@ -54,10 +55,16 @@ app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
+const url = 'https://eccomercedreamshop.herokuapp.com/';
 
-app.listen(
-  PORT,
+app.listen(PORT, () => {
+  // Default application refresh time is 25 mins
+  herokuAwake(url);
+
+  // If you want to change application refresh time
+  const time = 25;
+  herokuAwake(url, time);
   console.log(
     `Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
-  )
-);
+  );
+});
